4320f44bda33ead577a4d95ec46fb10e5a0fb99e
Drashna Jael're <drashna@live.com>
2024-06-02 16:41:04 -0700

    [TractylM] Add weact f405 support

--- keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/rules.mk
+++ keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/rules.mk
@@ -41,7 +41,15 @@ ifeq ($(strip $(KEYBOARD)), handwired/tractyl_manuform/5x6_right/f411)
     RTC_DRIVER                       = ds3231
     WATCHDOG_ENABLE                  = yes
 endif
-
+ifeq ($(strip $(KEYBOARD)), handwired/tractyl_manuform/5x6_right/f405)
+    AUTOCORRECT_ENABLE               = yes
+    LTO_SUPPORTED                    = no
+    OVERLOAD_FEATURES                = yes
+    HAPTIC_ENABLE                    = yes
+    HAPTIC_DRIVER                    = drv2605l
+    RTC_DRIVER                       = vendor
+    WATCHDOG_ENABLE                  = yes
+endif
 ifeq ($(strip $(OVERLOAD_FEATURES)), yes)
     AUDIO_ENABLE                     = yes
     CAPS_WORD_ENABLE                 = yes
