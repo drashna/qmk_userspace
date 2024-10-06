61b81312b98d7d02ca64bc079b18e8652cbdff42
Drashna Jael're <drashna@live.com>
2024-10-06 00:35:38 -0700

    [TractylM] Add f407 configuration
    [TractylM] Fix up keymap config
    
    [TractylM] Enable SPI flash eeprom on f407
    
    Add f407 to list of compile targets

--- keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/rules.mk
+++ keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/rules.mk
@@ -46,6 +46,21 @@ ifeq ($(strip $(KEYBOARD)), handwired/tractyl_manuform/5x6_right/f405)
         OPT_DEFS += -DPAL_USE_WAIT=TRUE -DPORTSCAN_MATRIX_ENABLE
     endif
 endif
+ifeq ($(strip $(KEYBOARD)), handwired/tractyl_manuform/5x6_right/f407)
+    OVERLOAD_FEATURES                     = yes
+
+    CONSOLE_ENABLE                        = yes
+    BACKLIGHT_ENABLE                      = yes
+    RGB_MATRIX_ENABLE                     = yes
+    RGBLIGHT_DRIVER                       = custom
+    QUANTUM_PAINTER_ENABLE                = yes
+    CUSTOM_QUANTUM_PAINTER_ENABLE         = yes
+    CUSTOM_QUANTUM_PAINTER_ILI9341        = yes
+    CUSTOM_QUANTUM_PAINTER_ILI9488        = no
+    CUSTOM_QUANTUM_PAINTER_ST7789_170X320 = no
+    CUSTOM_QUANTUM_PAINTER_ST7789_135X240 = no
+    WEAR_LEVELING_DRIVER                  = spi_flash
+endif
 
 ifeq ($(strip $(OVERLOAD_FEATURES)), yes)
     AUDIO_ENABLE                  = yes
@@ -58,8 +73,6 @@ ifeq ($(strip $(OVERLOAD_FEATURES)), yes)
     ENCODER_ENABLE                = yes
     ENCODER_MAP_ENABLE            = yes
     LTO_ENABLE                    = yes
-    MULTITHREADED_LIGHTING_ENABLE = yes
-    MULTITHREADED_PAINTER_ENABLE  = no
     OPT                           = fast
     RGBLIGHT_ENABLE               = yes
     RGBLIGHT_STARTUP_ANIMATION    = no
