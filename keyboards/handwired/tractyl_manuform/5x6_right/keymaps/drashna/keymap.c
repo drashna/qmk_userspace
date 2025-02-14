e88e9ed4b7d062713796cf0a6949f24338d9ca4b
Drashna Jael're <drashna@live.com>
2025-02-14 14:58:10 -0800

    [TractylM] Remove USB HS code in favor of it being at keyboard level

--- keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/keymap.c
+++ keyboards/handwired/tractyl_manuform/5x6_right/keymaps/drashna/keymap.c
@@ -6,8 +6,8 @@
 // USB Port Pin Definitions
 // black - GND
 // red - VCC
-// white - D- - A11
-// blue - D+ - A12
+// white - D- - A11 - B14
+// blue - D+ - A12 - B15
 // green - CC1
 // yellow - CC2
 
@@ -102,18 +102,6 @@ void keyboard_post_init_keymap(void) {
     qp_close_image(my_image);
 }
 #endif // QUANTUM_PAINTER_ENABLE &&  !CUSTOM_QUANTUM_PAINTER_ENABLE
-
-#ifdef USE_USB_OTG_HS_PORT
-#    pragma message("Using USB OTG HS Port")
-void early_hardware_init_post(void) {
-    // D-  white
-    palSetLineMode(
-        B14, PAL_MODE_ALTERNATE(12) | PAL_STM32_OTYPE_PUSHPULL | PAL_STM32_OSPEED_HIGHEST | PAL_STM32_PUPDR_FLOATING);
-    // D+  blue
-    palSetLineMode(
-        B15, PAL_MODE_ALTERNATE(12) | PAL_STM32_OTYPE_PUSHPULL | PAL_STM32_OSPEED_HIGHEST | PAL_STM32_PUPDR_FLOATING);
-}
-#endif
 
 void execute_user_button_action(bool state) {
     // for the double tap action of the reset pin
