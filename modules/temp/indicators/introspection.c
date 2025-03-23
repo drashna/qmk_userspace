23dd299c6333ac944610bb6776df0436d18e2f05
Drashna Jael're <drashna@live.com>
2025-03-23 00:07:24 -0700

    Added some test/temp modules for new hooks

---
+++ modules/temp/indicators/introspection.c
@@ -0,0 +1,14 @@
+// Copyright Pablo Martinez (@elpekenin) <elpekenin@elpekenin.dev>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <platforms/progmem.h>
+
+#define NUM_INDICATORS_RAW (sizeof(indicators) / sizeof(indicator_t))
+
+uint8_t indicators_count(void) {
+    return NUM_INDICATORS_RAW;
+}
+
+indicator_t get_indicator(uint8_t i) {
+    return *(indicator_t *)pgm_read_ptr(&(indicators[i]));
+}
