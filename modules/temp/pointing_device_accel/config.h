d158b1834ccdca5a9853497f22318b7991f995bb
Drashna Jael're <drashna@live.com>
2025-04-02 13:40:43 -0700

    Reconfigure pointing device accel for use of pointers
    *** SEGFAULT ***

--- modules/temp/pointing_device_accel/config.h
+++ modules/temp/pointing_device_accel/config.h
@@ -13,7 +13,7 @@
 // Crude hack to enable eeprom storage for accel config and prevent overlap with start of VIA eeprom storage
 #if defined(VIA_ENABLE)
 #    if !defined(VIA_EEPROM_CUSTOM_CONFIG_SIZE)
-#        define VIA_EEPROM_CUSTOM_CONFIG_SIZE (20)
+#        define VIA_EEPROM_CUSTOM_CONFIG_SIZE (17)
 #    else
 #        if defined(VIA_EEPROM_CUSTOM_CONFIG_ERROR_OVERRIDE)
 #            pragma message \
--- modules/temp/pointing_device_accel/pointing_device_accel_via.c
+++ modules/temp/pointing_device_accel/pointing_device_accel_via.c
@@ -5,7 +5,8 @@
 #include "pointing_device_internal.h"
 #include "via.h"
 
-_Static_assert(sizeof(g_pointing_device_accel_config) <= VIA_EEPROM_CUSTOM_CONFIG_SIZE, "Mismatch in via custom eeprom stored data");
+_Static_assert(sizeof(pointing_device_accel_config_t) <= VIA_EEPROM_CUSTOM_CONFIG_SIZE,
+               "Mismatch in via custom eeprom stored data");
 
 enum via_pointing_device_channel {
     id_maccel = 24,
@@ -50,57 +51,32 @@ void pointing_device_config_set_value(uint8_t *data) {
         case id_pointing_device_takeoff:
             {
                 uint16_t takeoff = COMBINE_UINT8(value_data[0], value_data[1]);
-
-                g_pointing_device_accel_config.takeoff =
-                    PROJECT_FROM_VIA(takeoff, MACCEL_VIA_TKO_MIN, MACCEL_VIA_TKO_MAX);
-#ifdef MACCEL_DEBUG
-                printf("MACCEL:via: TKO: %.3f grw: %.3f ofs: %.3f lmt: %.3f\n", g_pointing_device_accel_config.takeoff,
-                       g_pointing_device_accel_config.growth_rate, g_pointing_device_accel_config.offset,
-                       g_pointing_device_accel_config.limit);
-#endif
+                pointing_device_accel_set_takeoff(PROJECT_FROM_VIA(takeoff, MACCEL_VIA_TKO_MIN, MACCEL_VIA_TKO_MAX));
                 break;
             }
         case id_pointing_device_growth_rate:
             {
                 uint16_t growth_rate = COMBINE_UINT8(value_data[0], value_data[1]);
-
-                g_pointing_device_accel_config.growth_rate =
-                    PROJECT_FROM_VIA(growth_rate, MACCEL_VIA_GRO_MIN, MACCEL_VIA_GRO_MAX);
-#ifdef MACCEL_DEBUG
-                printf("MACCEL:via: tko: %.3f GRW: %.3f ofs: %.3f lmt: %.3f\n", g_pointing_device_accel_config.takeoff,
-                       g_pointing_device_accel_config.growth_rate, g_pointing_device_accel_config.offset,
-                       g_pointing_device_accel_config.limit);
-#endif
+                pointing_device_accel_set_growth_rate(
+                    (float)PROJECT_FROM_VIA(growth_rate, MACCEL_VIA_GRO_MIN, MACCEL_VIA_GRO_MAX));
                 break;
             }
         case id_pointing_device_offset:
             {
                 uint16_t offset = COMBINE_UINT8(value_data[0], value_data[1]);
-
-                g_pointing_device_accel_config.offset =
-                    PROJECT_FROM_VIA(offset, MACCEL_VIA_OFS_MIN, MACCEL_VIA_OFS_MAX);
-#ifdef MACCEL_DEBUG
-                printf("MACCEL:via: tko: %.3f grw: %.3f OFS: %.3f lmt: %.3f\n", g_pointing_device_accel_config.takeoff,
-                       g_pointing_device_accel_config.growth_rate, g_pointing_device_accel_config.offset,
-                       g_pointing_device_accel_config.limit);
-#endif
+                pointing_device_accel_set_offset(
+                    (float)PROJECT_FROM_VIA(offset, MACCEL_VIA_OFS_MIN, MACCEL_VIA_OFS_MAX));
                 break;
             }
         case id_pointing_device_limit:
             {
                 uint16_t limit = COMBINE_UINT8(value_data[0], value_data[1]);
-
-                g_pointing_device_accel_config.limit = PROJECT_FROM_VIA(limit, MACCEL_VIA_LMT_MIN, MACCEL_VIA_LMT_MAX);
-#ifdef MACCEL_DEBUG
-                printf("MACCEL:via: tko: %.3f grw: %.3f ofs: %.3f LMT: %.3f\n", g_pointing_device_accel_config.takeoff,
-                       g_pointing_device_accel_config.growth_rate, g_pointing_device_accel_config.offset,
-                       g_pointing_device_accel_config.limit);
-#endif
+                pointing_device_accel_set_limit((float)PROJECT_FROM_VIA(limit, MACCEL_VIA_LMT_MIN, MACCEL_VIA_LMT_MAX));
                 break;
             }
         case id_pointing_device_enabled:
             {
-                g_pointing_device_accel_config.enabled = value_data[0];
+                pointing_device_accel_enabled((bool)value_data[0]);
                 break;
             }
     }
@@ -115,9 +91,8 @@ void pointing_device_config_get_value(uint8_t *data) {
     switch (*value_id) {
         case id_pointing_device_takeoff:
             {
-                // uint16_t takeoff = (g_pointing_device_accel_config.takeoff - 0.5) / (4.5 / 6) * 10000;
                 uint16_t takeoff =
-                    PROJECT_TO_VIA(g_pointing_device_accel_config.takeoff, MACCEL_VIA_TKO_MIN, MACCEL_VIA_TKO_MAX);
+                    PROJECT_TO_VIA(pointing_device_accel_get_takeoff(), MACCEL_VIA_TKO_MIN, MACCEL_VIA_TKO_MAX);
                 value_data[0] = takeoff >> 8;
                 value_data[1] = takeoff & 0xFF;
                 break;
@@ -125,7 +100,7 @@ void pointing_device_config_get_value(uint8_t *data) {
         case id_pointing_device_growth_rate:
             {
                 uint16_t growth_rate =
-                    PROJECT_TO_VIA(g_pointing_device_accel_config.growth_rate, MACCEL_VIA_GRO_MIN, MACCEL_VIA_GRO_MAX);
+                    PROJECT_TO_VIA(pointing_device_accel_get_growth_rate(), MACCEL_VIA_GRO_MIN, MACCEL_VIA_GRO_MAX);
                 value_data[0] = growth_rate >> 8;
                 value_data[1] = growth_rate & 0xFF;
                 break;
@@ -133,7 +108,7 @@ void pointing_device_config_get_value(uint8_t *data) {
         case id_pointing_device_offset:
             {
                 uint16_t offset =
-                    PROJECT_TO_VIA(g_pointing_device_accel_config.offset, MACCEL_VIA_OFS_MIN, MACCEL_VIA_OFS_MAX);
+                    PROJECT_TO_VIA(pointing_device_accel_get_offset(), MACCEL_VIA_OFS_MIN, MACCEL_VIA_OFS_MAX);
                 value_data[0] = offset >> 8;
                 value_data[1] = offset & 0xFF;
                 break;
@@ -141,14 +116,14 @@ void pointing_device_config_get_value(uint8_t *data) {
         case id_pointing_device_limit:
             {
                 uint16_t limit =
-                    PROJECT_TO_VIA(g_pointing_device_accel_config.limit, MACCEL_VIA_LMT_MIN, MACCEL_VIA_LMT_MAX);
+                    PROJECT_TO_VIA(pointing_device_accel_get_limit(), MACCEL_VIA_LMT_MIN, MACCEL_VIA_LMT_MAX);
                 value_data[0] = limit >> 8;
                 value_data[1] = limit & 0xFF;
                 break;
             }
         case id_pointing_device_enabled:
             {
-                value_data[0] = g_pointing_device_accel_config.enabled;
+                value_data[0] = pointing_device_accel_get_enabled();
                 break;
             }
     }
@@ -160,7 +135,7 @@ __attribute__((weak)) void pointing_device_config_update(pointing_device_accel_c
 }
 
 __attribute__((weak)) void pointing_device_config_read(pointing_device_accel_config_t *config) {
-    via_read_custom_config(&g_pointing_device_accel_config, 0, sizeof(g_pointing_device_accel_config));
+    via_read_custom_config(config, 0, sizeof(pointing_device_accel_config_t));
 }
 
 void via_custom_value_command_kb(uint8_t *data, uint8_t length) {
@@ -177,7 +152,7 @@ void via_custom_value_command_kb(uint8_t *data, uint8_t length) {
                 pointing_device_config_get_value(value_and_data);
                 break;
             case id_custom_save:
-                pointing_device_config_update(&g_pointing_device_accel_config);
+                pointing_device_accel_config_update(pointing_device_accel_config_get_pointer());
                 break;
             default:
                 *command_id = id_unhandled;
