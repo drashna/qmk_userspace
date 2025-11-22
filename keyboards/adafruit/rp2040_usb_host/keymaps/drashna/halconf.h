a76e97e8f010e40488fa30706adc7c64973c3abc
Drashna Jael're <drashna@drashna.net>
2025-11-22 05:42:21 -0800

    Add feature rp2040 usb host (working)

---
+++ keyboards/adafruit/rp2040_usb_host/keymaps/drashna/halconf.h
@@ -0,0 +1,8 @@
+// Copyright 2025 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#define HAL_USE_PWM TRUE
+
+#include_next <halconf.h>
