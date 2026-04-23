5cc66f55fc0b5567efc111daf253a14f568c6388
Drashna Jael're <drashna@drashna.net>
2026-04-22 17:11:11 -0700

    [ADB2USB] Update awilhite keymap

---
+++ keyboards/converter/adb_usb/keymaps/awilhite/halconf.h
@@ -0,0 +1,9 @@
+// Copyright 2026 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#define HAL_USE_PWM TRUE
+#define HAL_USE_SPI TRUE
+
+#include_next <halconf.h>
