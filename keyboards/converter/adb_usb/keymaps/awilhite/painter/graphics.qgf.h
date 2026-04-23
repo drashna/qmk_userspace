64aed95fe026a06a985489360460cf3fd8ceb972
Drashna Jael're <drashna@drashna.net>
2026-04-22 17:11:11 -0700

    [ADB2USB] Update awilhite keymap

---
+++ keyboards/converter/adb_usb/keymaps/awilhite/painter/graphics.qgf.h
@@ -0,0 +1,42 @@
+// Copyright 2022 QMK -- generated source code only, image retains original copyright
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <qp.h>
+
+extern const uint32_t gfx_apple_logo_length;
+extern const uint8_t  gfx_apple_logo[436];
+
+extern const uint32_t gfx_windows_logo_length;
+extern const uint8_t  gfx_windows_logo[404];
+
+extern const uint32_t gfx_linux_logo_length;
+extern const uint8_t  gfx_linux_logo[304];
+
+extern const uint32_t gfx_gamepad_24x24_length;
+extern const uint8_t  gfx_gamepad_24x24[102];
+
+extern const uint32_t gfx_mouse_24x24_length;
+extern const uint8_t  gfx_mouse_24x24[96];
+
+extern const uint32_t gfx_mouse_icon_length;
+extern const uint8_t  gfx_mouse_icon[68];
+
+extern const uint32_t gfx_alt_icon_length;
+extern const uint8_t  gfx_alt_icon[59];
+
+extern const uint32_t gfx_command_icon_length;
+extern const uint8_t  gfx_command_icon[59];
+
+extern const uint32_t gfx_control_icon_length;
+extern const uint8_t  gfx_control_icon[59];
+
+extern const uint32_t gfx_shift_icon_length;
+extern const uint8_t  gfx_shift_icon[59];
+
+extern const uint32_t gfx_windows_icon_length;
+extern const uint8_t  gfx_windows_icon[59];
+
+extern const uint32_t gfx_qmk_powered_by_length;
+extern const uint8_t  gfx_qmk_powered_by[3032];
