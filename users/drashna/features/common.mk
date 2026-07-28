e8daf99a474d1fa5e57b2f75f15c33954cbe4047
Drashna Jael're <drashna@drashna.net>
2026-07-28 10:39:08 -0700

    Add hacky sendstring replacement

--- users/drashna/features/common.mk
+++ users/drashna/features/common.mk
@@ -1,2 +1,10 @@
 -include $(USER_PATH)/features/rtc/rtc.mk
 VPATH += $(USER_PATH)/features/
+
+CUSTOM_SEND_STRING_ENABLE ?= yes
+ifeq ($(strip $(CUSTOM_SEND_STRING_ENABLE)), yes)
+    OPT_DEFS += -DCUSTOM_SEND_STRING_ENABLE -DSEND_STRING_ENABLE
+    override SEND_STRING_ENABLE = no
+    VPATH += $(USER_PATH)/features/custom_send_string/
+    SRC += $(USER_PATH)/features/custom_send_string/send_string.c
+endif
