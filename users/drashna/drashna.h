d4fa7a888c25ad04797d7c3ddc46866c537b1e11
Drashna Jael're <drashna@drashna.net>
2026-09-22 20:52:06 -0700

    Remove unneeded rgb matrix function fron non-rgbmatrix files

--- users/drashna/drashna.h
+++ users/drashna/drashna.h
@@ -15,11 +15,6 @@
 #    include "keyrecords/custom_tap_dance.h"
 #endif // CUSTOM_TAP_DANCE_ENABLE
 
-#if defined(RGB_MATRIX_ENABLE)
-void rgb_matrix_layer_helper(uint8_t hue, uint8_t sat, uint8_t val, uint8_t mode, uint8_t speed, uint8_t led_type,
-                             uint8_t led_min, uint8_t led_max);
-#endif // defined(RGB_MATRIX_ENABLE)
-
 void     matrix_scan_rate_task(void);
 uint32_t get_matrix_scan_rate(void);
 
